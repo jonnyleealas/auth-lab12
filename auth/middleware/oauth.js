9cdbf8354a5d347d3788331991ddb989012d593e
jonathon lee <jonnyleealas@gmail.com>
2020-09-21 18:53:59 -0700

    githuboauthworks1

--- auth/middleware/oauth.js
+++ auth/middleware/oauth.js
@@ -1,12 +1,56 @@
 'use strict';
+const superagent = require('superagent');
 
-module.exports = (req,res,next)=>{
-  console.log(req.query);
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const TOKEN_SERVER = process.env.TOKEN_SERVER;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const STATE = process.env.STATE;
+const REMOTE_API = process.env.REMOTE_API;
+
+module.exports = async (req,res,next)=>{
+ //   // Get the code
+  let code = req.query.code;
+  console.log('(1) CODE:', code)
+
+//   // Exchange the code for a token
+  let remoteToken = await exchangeCodeForToken(code);
+  console.log('(2)', remoteToken)
+
+//   // Get user info from GitHub
+  // let remoteUser = await getRemoteUser(remoteToken);
+  // console.log('(3)', remoteUser);
+  // Connect that with our database
+
+  // let localUser = await getLocalUser(remoteUser.login)
+  // console.log('(4)', localUser);
+
   next();
 }
 
 
 
+async function exchangeCodeForToken(code){
+  let tokenResponse = await superagent.post(TOKEN_SERVER)
+  .send({
+    code: code,
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+    redirect_uri: REDIRECT_URI,
+    state: STATE,
+    grand_type: 'authorization_code'
+
+  })
+  let access_token = tokenResponse.body.access_token;
+  return access_token
+
+}
+// async function getRemoteUser(token){
+
+// }
+// async function getLocalUser(){
+
+// }
 
 
 
@@ -22,31 +66,9 @@ module.exports = (req,res,next)=>{
 
 
 
-// const superagent = require('superagent');
 
-// const CLIENT_ID = process.env.CLIENT_ID;
-// const CLIENT_SECRET = process.env.CLIENT_SECRET;
-// const TOKEN_SERVER = process.env.TOKEN_SERVER;
-// const REDIRECT_URI = process.env.REDIRECT_URI;
-// const STATE = process.env.STATE;
-// const REMOTE_API = process.env.REMOTE_API;
 // module.exports = async (req, res, next) => {
 
-//   // Get the code
-//   let code = req.query.code;
-//   console.log('(1) CODE:', code)
-
-//   // Exchange the code for a token
-//   let remoteToken = await exchangeCodeForToken(code);
-//   console.log('(2)', remoteToken)
-
-//   // Get user info from GitHub
-//   let remoteUser = await getRemoteUser(remoteToken);
-//   console.log('(3)', remoteUser);
-//   // Connect that with our database
-
-//   let localUser = await getLocalUser(remoteUser.login)
-//   console.log('(4)', localUser);
 
 //   next();
 // }
